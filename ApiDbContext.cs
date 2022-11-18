7f2ec500bd492ed8ebfe20bad87f9dc94b0c2095
devpedroxavier <106628690+devpedroxavier@users.noreply.github.com>
2022-11-18 15:12:20 -0300

    Add files via upload

---
+++ ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Farmaco> Farmaco { get; set; }
+    }
+}
---
+++ Class.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Farmaco {
+        public string nome { get; set; }
+        public int id { get; set; }
+        public decimal preco { get; set; }
+        public string laboratorio { get; set; }
+  }
+    
+}
